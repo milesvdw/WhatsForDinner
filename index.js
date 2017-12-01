b622cd1707809cf1e983a77f5dd401093d7ae65e
Miles Van de Wetering <miles.vdw@gmail.com>
2017-11-30 20:05:03 -0800

    Switched index methods

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(request, response) {
+//.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.htm");
+})
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Howdy World!");
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
 
-});
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+})
