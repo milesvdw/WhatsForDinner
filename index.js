44ba31e643a3e51b983347e67d31a06191cbe9d2
Miles Van de Wetering <miles.vdw@gmail.com>
2017-11-30 21:36:51 -0800

    Fixed index path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 //.use(express.static('public'));
 app.get('/', function (req, res) {
-    res.sendFile("index.htm");
+    res.sendFile("/index.htm");
 })
 
 app.get('/process_get', function (req, res) {
@@ -16,7 +16,7 @@ app.get('/process_get', function (req, res) {
     res.end(JSON.stringify(response));
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(1337, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
