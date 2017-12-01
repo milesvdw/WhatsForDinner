f45bb4af0bd3133ec68a3ede5b131458c9cf9187
Miles Van de Wetering <miles.vdw@gmail.com>
2017-11-30 20:36:33 -0800

    Simplified to SAP pattern

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 //.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
-    res.sendFile(__dirname + "/" + "index.htm");
+app.get('/', function (req, res) {
+    res.sendFile("index.htm");
 })
 
 app.get('/process_get', function (req, res) {
